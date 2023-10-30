a18583fbf65d8d53e6135503bd0dd30d4c1be5cd
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2023-10-30 02:31:29 -0500

    ADD [New routes for the game]

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,22 +1,32 @@
 import express from "express";
 import { registerUser, loginUser, recoverPassword, getAllUsers ,modifyInformation, logout, deleteUser} from '../controllers/Users/authController'
-import { usersValidation, loginValidations , recoverPasswordValidations} from '../middlewares/userValidations'
-import { authenticateToken } from '../middlewares/protectedRoutes'
-import { getAllFailedAttempts, verificationCode, codeConfirm, getAllCode } from "../controllers/FailedLogin/authController";
-import * as gamesController from '../controllers/Questions/gameController'
+import {
+  usersValidation,
+  loginValidations,
+  recoverPasswordValidations,
+  addScoreValidation,
+} from "../middlewares/userValidations";
+import { authenticateToken } from "../middlewares/protectedRoutes";
+import {
+  getAllFailedAttempts,
+  verificationCode,
+  codeConfirm,
+  getAllCode,
+} from "../controllers/FailedLogin/authController";
+import * as gamesController from "../controllers/Questions/gameController";
 const router = express.Router();
 
 //Users
-router.get('/users',getAllUsers)
-router.post('/register', usersValidation(), registerUser)
-router.post('/login', loginValidations(), loginUser)
-router.post('/logout', logout)
-router.put('/recover-password', recoverPasswordValidations(), recoverPassword)
-router.put('/update-user', modifyInformation)
-router.delete('/delete/:userId',deleteUser)
+router.get("/users", getAllUsers);
+router.post("/register", usersValidation(), registerUser);
+router.post("/login", loginValidations(), loginUser);
+router.post("/logout", logout);
+router.put("/recover-password", recoverPasswordValidations(), recoverPassword);
+router.put("/update-user", modifyInformation);
+router.delete("/delete/:userId", deleteUser);
 
 // Protected Routed
-router.get("/ruta-protegida", authenticateToken, (req, res) => {
+router.get("/protected-route", authenticateToken, (req, res) => {
   // La solicitud solo llegará aquí si el token es válido
   res.json({ message: "Acceso al recurso protegido permitido" });
 });
@@ -32,8 +42,15 @@ router.get("/save-questions", gamesController.saveQuestions);
 router.get("/get-questions", gamesController.getAllQuestions);
 router.get("/get-question/:id", gamesController.getQuestionById);
 router.get("/questions/:level", gamesController.filterQuestionsByLevel);
+router.get("/score-history/:userId", gamesController.getScoreHistory);
+router.get("/call-friend/:questionId", gamesController.callToAFriend);
 router.post("/question/answer/:questionId", gamesController.userResponse);
 router.post("/question/public-help/:id", gamesController.publicHelp);
+router.post(
+  "/question/user/score",
+  addScoreValidation(),
+  gamesController.addScoreToUser,
+);
 
 
 
